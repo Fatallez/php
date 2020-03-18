88bd318f4019fee82f0d855bc63baf38568a67f6
Fatallez_VT <fatallez@rambler.ru>
2020-03-18 20:44:38 +0400

    /

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $h1 = 'Current year: ';
+    $title = 'GBsite';
+    $date = date(Y);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <title><?php echo $title ?></title>
+</head>
+<body>
+<h1><?php echo $h1 . $date ?></h1>
+</body>
+</html>
