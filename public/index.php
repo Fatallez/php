92ef7fa22e83aa0443319ad60cbcfe5a176d03c2
Fatallez_VT <fatallez@rambler.ru>
2020-04-22 20:35:58 +0400

    /

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
+use \App\services\DB;
+
 include dirname(__DIR__) . '/services/Autoloader.php';
 spl_autoload_register([new Autoloader(), 'loadClass']);
 
-$bd = new \App\services\DB();
+$bd = new DB();
 $user = new \App\models\Basket($bd);
 echo $user->getAll();
 echo '<br>';
